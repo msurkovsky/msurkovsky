6d1817660844decad004abdcb337f0f75b13d51e
Martin Surkovsky <martin.surkovsky@gmail.com>
2025-04-01 18:04:02 +0200

    Set base in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: './',  // Add this line to make it work with GitHub Pages
+  base: '/msurkovsky/',  // Add this line to make it work with GitHub Pages
   plugins: [
     react(),
     mode === 'development' &&
